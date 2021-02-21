d9ead4c4c125dfd0f34875f1a0522ec27a4b2ab1
Sasha Lukas <sashamati@gmail.com>
2021-02-21 10:51:22 -0800

    feat: added mock components for posts, header, added image.

--- README.md
+++ README.md
@@ -70,5 +70,5 @@ This section has moved here: [https://facebook.github.io/create-react-app/docs/d
 This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
 
 [] Setup firebase 🚀
-[] clean the create-react app initial setup
+[√] clean the create-react app initial setup
 [] markup code for design
--- src/App.js
+++ src/App.js
@@ -2,10 +2,22 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1> Hello this is instagram 🚀 </h1>
-  
+    <div className="app">
+     
 
+      <div className="app__header">
+        <img src="https://logos-world.net/wp-content/uploads/2020/04/Instagram-Logo.png" 
+        alt="" 
+        className="app__headerImage"/>
+      </div>
+      <h1> Hello this is instagram 🚀 </h1>
+      <h1> Hello this is instagram 🚀 </h1>
+      <h1> Hello this is instagram 🚀 </h1>
+      <h1> Hello this is instagram 🚀 </h1>
+
+
+      {/* Posts */}
+      {/* Posts */}
     </div>
   );
 }
