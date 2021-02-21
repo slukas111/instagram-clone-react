5ecf6afc63f839365701de4df79ebb5e9d91de79
Sasha Lukas <sashamati@gmail.com>
2021-02-21 14:26:15 -0800

    style: arranged the look in post and app css files, margin borders and implemented material-ui

--- src/App.js
+++ src/App.js
@@ -6,22 +6,19 @@ function App() {
   return (
     <div className="app">
       <div className="app__header">
-
         <img 
         src="/images/insta-logo.png"
         className="app__headerImage"
         alt="" 
         />
+
       </div>
-      <h1> Hello this is instagram 🚀 </h1>
-      <h1> Hello this is instagram 🚀 </h1>
-      <h1> Hello this is instagram 🚀 </h1>
-      <h1> Hello this is instagram 🚀 </h1>
-
-
+        <h1> Hello, username welcome to instagram! :rocket</h1>
       <Posts />
-      {/* Posts */}
+      <Posts />
+      <Posts />
     </div>
+
 
   );
 }
--- src/Posts.js
+++ src/Posts.js
@@ -1,17 +1,24 @@
 import React from 'react'
 import './Post.css';
+import Avatar from "@material-ui/core/Avatar"
 
 function Posts() {
     return (
         <div className="post">
-            <h3>Username: </h3>
+            <div className="post__header">
+
+            <Avatar 
+            className ="post__avatar"
+            alt="Remy Sharp" 
+            src="/static/images/avatar/1.jpg" />
+            <h3>Username</h3>
           {/* header -> avatar + username  */}
+          </div>
 
           <img className="post__image"
           src="https://www.impactbnd.com/hubfs/blog-files/find-micro-influencers-on-instagram.jpeg" 
           alt=""/>
         <h4 className="post__text"> <strong>Username:</strong> [[Caption]]</h4>
-
         </div>
     )
 }
