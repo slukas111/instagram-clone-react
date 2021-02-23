9a1ddca77b4af71162f5698579e215259168c8ae
Sasha Lukas <sashamati@gmail.com>
2021-02-23 10:13:44 -0800

    feat: import material ui modal and applied modal for sign up button

--- src/App.js
+++ src/App.js
@@ -2,53 +2,95 @@ import React, { useState, useEffect  } from 'react';
 import './App.css';
 import Posts from './Posts';
 import { db } from './firebase';
+import { makeStyles } from '@material-ui/core/styles';
+import Modal from '@material-ui/core/Modal';
+import { Button } from '@material-ui/core';
+
+
+function getModalStyle() {
+  const top = 50;
+  const left = 50;
+  
+  return {
+    top: `${top}%`,
+    left: `${left}%`,
+    transform: `translate(-${top}%, -${left}%)`,
+  };
+}
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    position: 'absolute',
+    width: 400,
+    backgroundColor: theme.palette.background.paper,
+    border: '2px solid #000',
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+  },
+}));
+
 
 
 function App() {
-  const [posts, setPosts] = useState([
-    // {
-    //   username: "user1",
-    //   caption: "dummy text for post 1",
-    //   imageUrl: "https://daman.co.id/daman.co.id/wp-content/uploads/2016/06/10-male-models-to-follow-on-instagram_tomas-skoloudik-1024x1024.jpg"
-    // },
-    // {
-    //   username: "user2",
-    //   caption: "dummy text for post 2", 
-    //   imageUrl: "http://www.modzik.com/wp-content/uploads/2015/05/Hot-Men-Coffee-Instagram-Pictures-6.jpg"
-    // },
-    // {
-    //   username: "user3", 
-    //   caption: "dummy text for post 3",
-    //   imageUrl: "https://qnews.com.au/wp-content/uploads/2019/04/19-04-09-Queer-Eye-Jess.jpeg"
-    // }
-  ]);
-  
+  // firebase database posts
+  const [posts, setPosts] = useState([]);
+  // modal defined
+  const classes = useStyles();
+  const [open, setOpen] = useState(false);
+  const [modalStyle] = useState(getModalStyle);
+
+
+  //  run once database collection, maping posts + id  
   useEffect(() => {
     db.collection('posts').onSnapshot(snapshot => {
-      setPosts(snapshot.docs.map(doc => doc.data()));
+      setPosts(snapshot.docs.map(doc => ({
+        id: doc.id,
+        post: doc.data()
+      })));
     })
   }, []);
+  
+  // sign up and login functions
+  const signUp = (event) => {
+    
+  }
+  
 
   return (
     <div className="app">
+            <Modal
+              open={open}
+              onClose={() => setOpen(false)}
+            >
+            <div style={modalStyle} className={classes.paper}>
+                <center>
+                <img 
+                  src="/images/insta-logo.png"
+                  className="app__headerImage"
+                  alt="Instagram Logo" 
+                />
+                </center>
+            </div>
+            </Modal>
+      
       <div className="app__header">
         <img 
         src="/images/insta-logo.png"
         className="app__headerImage"
-        alt="" 
+        alt="Instagram Logo" 
         />
-
       </div>
+      <Button onClick = {() => setOpen(true)}>Sign Up</Button>
+
         <h1> Hello,  welcome to instagram! 🚀</h1>
         {
-          posts.map(post => (
-            <Posts username={post.username} caption={post.caption} imageUrl={post.imageUrl} />
+          posts.map(({id, post}) => (
+            <Posts key={id} username={post.username} caption={post.caption} imageUrl={post.imageUrl} />
             ))
           }
           </div>
 
-
-  );
+);
 }
 
 export default App;
